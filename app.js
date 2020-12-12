230282cb529aaf3a8019ee9ec0a32cee06c891d5
karankc27 <karankaramchandani5@gmail.com>
2020-12-12 18:55:31 +0530

    Created To Do List App

---
+++ app.js
@@ -0,0 +1,30 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const request=require("request");
+const app=express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+var items=[];
+
+app.set("view engine","ejs");
+app.get("/",function(req,res){
+  var today=new Date();
+  var options={
+    weekday:"long",
+    day:"numeric",
+    month:"long"
+  } ;
+  var day=today.toLocaleDateString("en-US",options);
+
+res.render("list",{kindofday:day,newListItem: items});
+})
+
+app.post("/",function(req,res){
+  var item= req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+});
+
+app.listen(3000,function(){
+  console.log("Running on 3000");
+});
